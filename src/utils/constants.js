b8bfc8c974f8bb09ab1881cdceadd985f6f1076c
Manasa R <manasa.r@kyndryl.com>
2023-11-28 15:49:57 +0530

    Fixing cors policy issue

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const LOGO_URL = " https://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg";
 export const MENU_API =
- "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0396218&lng=77.5194387&restaurantId=";
+ "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0396218&lng=77.5194387&restaurantId=";
 
  //https://www.designevo.com/res/templates/thumb_small/chef-hat-mustache-waffle.png
